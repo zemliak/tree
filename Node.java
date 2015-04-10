3e756801e550e02c430ebf6e4882c56ccd1a9e36
Nikita A Zemlevskiy <zemliak@users.noreply.github.com>
2015-04-10 06:31:17 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,18 +1,18 @@
 package tree;
 
 public class Node {
-	Object obj;
+	Integer x;
 	Node child1;
 	Node child2;
-	public Node(Object obj, Node child1, Node child2){
-		this.obj = obj;
+	public Node(Integer x, Node child1, Node child2){
+		this.x = x;
 		this.child1 = child1;
 		this.child2 = child2;
 	}
-	public Node(Object obj, Node child1){
-		this(obj, child1, null);
+	public Node(Integer x, Node child1){
+		this(x, child1, null);
 	}
-	public Node(Object obj){
-		this(obj, null, null);
+	public Node(Integer x){
+		this(x, null, null);
 	}
 }
